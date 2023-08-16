ef7843bc49c6d0e4b2f2b3105481224c370ccaac
Redddeve <127204854+Redddeve@users.noreply.github.com>
2023-08-16 18:03:06 +0000

    Deploying to gh-pages from @ Redddeve/goit-js-hw-09@02d00204a522d0816111677091ac0fd8cc095ca9 🚀

--- 01-color-switcher.47123eb2.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=!1,r=null;function o(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){if(n)return;n=!0,document.body.style.backgroundColor=o(),r=setInterval((()=>{document.body.style.backgroundColor=o()}),1e3)})),e.addEventListener("click",(function(){if(!n)return;clearInterval(r)}));
-//# sourceMappingURL=01-color-switcher.47123eb2.js.map
---
+++ 01-color-switcher.de410141.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let n=!1,r=null;function o(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){if(n)return;n=!0,document.body.style.backgroundColor=o(),r=setInterval((()=>{document.body.style.backgroundColor=o()}),1e3)})),e.addEventListener("click",(function(){if(!n)return;clearInterval(r),n=!1}));
+//# sourceMappingURL=01-color-switcher.de410141.js.map
